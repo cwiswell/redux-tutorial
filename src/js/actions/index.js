5a6329d2189cd0c9e379c2c19a60c5412dd620d2
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-10 13:25:17 -0400

    Update action to use new actiontype const

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,3 +1,6 @@
+// using this const/enum will prevent typos from the action types
+import { ADD_ARTICLE } from '../constants/action-types';
+
 export function addArticle(payload) {
-    return { type: "ADD_ARTICLE", payload }
+    return { type: ADD_ARTICLE, payload }
 };
