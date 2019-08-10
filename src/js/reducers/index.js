2684c674e007b134acc05ee7a4e5248a0623b8cf
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-10 13:29:49 -0400

    Update reducer to use action

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,20 @@
+import {ADD_ARTICLE} from "../constants/action-types";
+
 const initialState ={
     articles: []
 };
 
 function rootReducer(state = initialState, action){
+    if(action.type === ADD_ARTICLE){
+        //This breaks the main principle of redux, state is immutable
+        //state.articles.push(action.payload);
+
+        //This is the redux way to return state
+        return Object.assign({}, state, {
+            articles: state.articles.concat(action.payload)
+        });
+    }
+
     return state;
 }
 
